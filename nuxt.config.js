b2ca2539bfdda14e3b87bcee1a1d5f258d482091
gitamgadtaula <gitamgadtaula@gmail.com>
2021-04-02 14:55:55 +0545

    connected axios with json server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
@@ -6,28 +6,24 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - vuetify-website',
-    title: 'vuetify-website',
+    titleTemplate: "%s - vuetify-website",
+    title: "vuetify-website",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -35,16 +31,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
+  modules: ["@nuxtjs/axios"],
+  axios: {
+    baseUrl: "http://localhost:1999/"
+  },
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: false,
       themes: {
@@ -62,6 +59,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -3,15 +3,31 @@
     <br />
     <br />
     <br />
-    <v-row>
-      <v-file-input
-        label="File input"
-        filled
-        prepend-icon="mdi-camera"
-        @change="imageChange"
-      ></v-file-input>
-      <img :src="image" />
-    </v-row>
+    <!-- <v-row> -->
+    <v-card elevation="2" class="mx-auto">
+      <v-card-title>Enter new item </v-card-title>
+      <v-form ref="form" lazy-validation style="padding: 20px">
+        <v-text-field v-model="form.value" label="Title" required></v-text-field>
+        <v-text-field
+          v-model="form.desc"
+          label="Description"
+          required
+        ></v-text-field>
+        <v-file-input
+          label="Upload Image"
+          filled
+          prepend-icon="mdi-camera"
+          @change="imageChange"
+        ></v-file-input>
+        <v-btn color="blue-grey" class="ma-2 white--text" @click="submitPost">
+          Submit
+          <v-icon right dark> mdi-cloud-upload </v-icon>
+        </v-btn>
+      </v-form>
+    </v-card>
+
+    <img :src="form.image" />
+    <!-- </v-row> -->
   </div>
 </template>
 
@@ -19,16 +35,24 @@
 export default {
   data() {
     return {
-      image: "",
+      form: {
+        image: "",
+        value: "",
+        desc: "",
+      },
     };
   },
   methods: {
+    submitPost() {
+      this.$axios.post("/projekte", this.form).then((response) => {
+        console.log(response);
+      });
+    },
     imageChange(e) {
       this.getBase64(e, (base64Data) => {
         console.log("Base 64 data is: " + base64Data);
-        this.image = base64Data;
+        this.form.image = base64Data;
       });
-      //   console.log(e);
     },
     getBase64(file, callback) {
       const reader = new FileReader();
