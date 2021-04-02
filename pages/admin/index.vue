663ccfd72cf6a890fb9ffd6676a740b1d530491c
Gitam Gadtaula <gitamgadtaula@gmail.com>
2021-04-02 09:09:47 +0545

    base 64 image conversion for image store in json file

---
+++ pages/admin/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <br />
+    <br />
+    <br />
+    <v-row>
+      <v-file-input
+        label="File input"
+        filled
+        prepend-icon="mdi-camera"
+        @change="imageChange"
+      ></v-file-input>
+      <img :src="image" />
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      image: "",
+    };
+  },
+  methods: {
+    imageChange(e) {
+      this.getBase64(e, (base64Data) => {
+        console.log("Base 64 data is: " + base64Data);
+        this.image = base64Data;
+      });
+      //   console.log(e);
+    },
+    getBase64(file, callback) {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => callback(reader.result));
+      reader.readAsDataURL(file);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
