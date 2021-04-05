7becef60d94117f534cf492ef26fbdbadb9a48b1
gitamgadtaula <gitamgadtaula@gmail.com>
2021-04-05 22:36:35 +0545

    git credentials change

---
+++ pages/admin/projekte copy 2/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view-post type="projekte" />
+</template>
+
+<script>
+import ViewPost from "@/components/Admin/ViewPost";
+export default {
+  components: { ViewPost },
+};
+</script>
+
+<style>
+</style>
